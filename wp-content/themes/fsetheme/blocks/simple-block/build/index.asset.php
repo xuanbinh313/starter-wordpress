0b4a0d6cd10452b2b322ba638b06e398414e26c1
BinhTX <binh.xuantran@digicommercegroup.com>
2025-05-08 09:48:37 +0700

    add css to header block

--- wp-content/themes/fsetheme/blocks/simple-block/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-blocks', 'wp-i18n'), 'version' => 'ddb0841ced1ed264408e');
--- wp-content/themes/fsetheme/blocks/static-block/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-blocks', 'wp-i18n', 'wp-server-side-render'), 'version' => 'ef6185664e543ce043fb');
